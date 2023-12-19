f861b248cf15040d4fb18e75ba4ca9dc48ab2549
Sam Selikoff <sam.selikoff@gmail.com>
2023-12-19 11:49:08 -0500

    wip

---
+++ app/components.tsx
@@ -0,0 +1,176 @@
+import { motion, useAnimation } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { z } from 'zod';
+
+interface ProgressBarProps {
+  progress: number; // progress in percentage
+  totalSteps: number; // total number of steps
+  currentStep: number; // current step
+}
+
+interface StepProps {
+  step: number;
+  currentStep: number;
+  stepPercentage: number;
+  overallProgressPercentage: number;
+  totalSteps: number;
+  height: number;
+  stepScale: number;
+}
+
+const StepPropsSchema = z.object({
+  stepPercentage: z.number().min(0).max(1),
+  overallProgressPercentage: z.number().min(0).max(1),
+  totalSteps: z.number().int().min(0),
+  currentStep: z.number().int().min(0),
+  step: z.number().int().min(0),
+  height: z.number().min(0),
+  stepScale: z.number().min(0),
+});
+
+const DotColors = {
+  completed: `var(--brand-accent)`,
+  current: `var(--brand-primary)`,
+  future: `var(--gray-300)`,
+};
+
+const BarColors = {
+  // complete: "var(--brand-primary-light)",
+  complete: `var(--brand-accent)`,
+  incomplete: `var(--gray-300)`,
+};
+
+function validateProps(props: any, schema: any): StepProps {
+  const result = schema.safeParse(props);
+
+  if (result.success) {
+    return result.data;
+  } else {
+    console.error(result.error);
+    throw new Error(`Invalid props`);
+  }
+}
+function Step(props: StepProps) {
+  const validatedProps = validateProps(props, StepPropsSchema);
+  const {
+    step,
+    stepPercentage,
+    overallProgressPercentage,
+    currentStep,
+    totalSteps,
+    height,
+    stepScale,
+  } = validatedProps;
+
+  let color = DotColors.future;
+  if (Math.abs(stepPercentage - overallProgressPercentage) <= 0.005) {
+    color = DotColors.current;
+  } else if (stepPercentage < overallProgressPercentage) {
+    color = DotColors.completed;
+  }
+  const [initialColor] = useState(color);
+  const h = height * stepScale;
+
+  return (
+    <motion.div
+      key={step}
+      style={{
+        position: `absolute`,
+        top: `50%`,
+        left: `${(step / totalSteps) * 100}%`, // Position the dot based on the step
+        transform: `translate(-50%, -50%)`,
+        height: h,
+        width: h,
+        borderRadius: `50%`,
+        background: color,
+      }}
+      initial={{
+        left: `${(step / totalSteps) * 100}%`,
+        background: initialColor, // Initial color
+      }}
+      animate={{
+        // left: `${(step / totalSteps) * 100}%`,
+        background: color, // Color to animate to
+      }}
+      transition={{
+        duration: 0.1,
+        ease: `easeInOut`, // Add easing function here
+        background: { duration: 0.2, ease: `easeInOut` }, // Specify easing for color transition
+      }}
+    ></motion.div>
+  );
+}
+export const AnimatedProgressBar: React.FC<ProgressBarProps> = ({
+  progress,
+  totalSteps,
+  currentStep,
+}) => {
+  if (currentStep < 0) return null;
+
+  const percentage = progress * 100;
+
+  // Create an array of steps
+  const zeroedStepsArray = Array.from({ length: totalSteps + 1 }, (_, i) => {
+    return {
+      index: i,
+      progressPercentage: i / totalSteps,
+    };
+  });
+
+  const [animatedProgress, setAnimatedProgress] = useState(progress);
+
+  const height = 8;
+  const stepScale = 1.75;
+  return (
+    <div
+      className={`mx-4`}
+      style={{ position: `relative`, height, marginTop: height * stepScale }}
+    >
+      <div
+        style={{
+          position: `absolute`,
+          top: 0,
+          left: 0,
+          height,
+          width: `100%`,
+          backgroundColor: BarColors.incomplete,
+        }}
+      ></div>
+      <motion.div
+        style={{
+          position: `absolute`,
+          top: 0,
+          left: 0,
+          height, // Height of the top border
+          borderTopRightRadius: height * 0.5, // Radius of the top border,
+          borderBottomRightRadius: height * 0.5, // Radius of the top border,
+          background: BarColors.complete, // Gradient color
+          width: `${percentage}%`, // Set width based on progress
+        }}
+        onUpdate={(latest) => {
+          const percentString = latest.width.toString();
+          const w = parseFloat(percentString.replace(`%`, ``));
+          // should I bet using doing a setState with the value of w?
+          setAnimatedProgress(w / 100);
+        }}
+        initial={{ width: 0 }} // Initial state of the animation
+        animate={{ width: `${percentage}%` }} // End state of the animation
+        transition={{ duration: 0.5 }} // Transition properties
+      />
+      {zeroedStepsArray.map((step) => {
+        return (
+          <Step
+            key={step.index}
+            step={step.index}
+            height={height}
+            stepScale={stepScale}
+            stepPercentage={step.progressPercentage}
+            overallProgressPercentage={animatedProgress}
+            currentStep={currentStep}
+            totalSteps={totalSteps}
+          />
+        );
+      })}
+    </div>
+  );
+};
---
+++ app/routes/_index.tsx
@@ -0,0 +1,50 @@
+import type { MetaFunction } from '@remix-run/node';
+import { AnimatedProgressBar } from '../components';
+import { useState } from 'react';
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
+  ];
+};
+
+const totalSteps = 10;
+
+export default function Index() {
+  const [step, setStep] = useState(5);
+  const zeroedTotalPages = totalSteps;
+  const zeroedCurrentPage = step;
+  const zeroedProgressPercentage = step / totalSteps;
+
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <div className="py-4 flex gap-x-8 px-4">
+        <button
+          type="button"
+          className="bg-blue-200 px-1"
+          onClick={() => {
+            setStep((prev) => Math.max(0, prev - 1));
+          }}
+        >
+          back
+        </button>
+        <button
+          type="button"
+          className="bg-blue-200 px-1"
+          onClick={() => {
+            setStep((prev) => Math.min(zeroedTotalPages, prev + 1));
+          }}
+        >
+          forward
+        </button>
+      </div>
+      <div className="w-full bg-gray-200 py-4">
+        <AnimatedProgressBar
+          progress={zeroedProgressPercentage}
+          totalSteps={zeroedTotalPages}
+          currentStep={zeroedCurrentPage}
+        />
+      </div>
+    </div>
+  );
+}
